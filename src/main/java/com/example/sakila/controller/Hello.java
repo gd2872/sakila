af1a99d3da508423fbd00665bc448387f52e9a9d
GD <GD@gdj>
2024-10-31 12:06:30 +0900

    로그 설정 추가

---
+++ src/main/java/com/example/sakila/controller/Hello.java
@@ -0,0 +1,19 @@
+package com.example.sakila.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j // 이 클래스에서 log라는 변수를 사용 가능 (객체 자동 주입)
+@Controller
+public class Hello {
+	@GetMapping("/hello")
+	public String hello() {
+		// System.out.println("디버깅 코드");
+		// loggin 프레임워크 사용
+		// Log log = new Log(); 원래 이렇게 사용해야하지만
+		log.debug("ㅎㅎ"); // @Slf4j때문에 사용 가능함
+		return "hello";
+	}
+}
